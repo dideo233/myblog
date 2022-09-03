fa6250bc55c4d9dc7901a8f7600fd0e4c872e38a
dideo233 <61315671+dideo233@users.noreply.github.com>
2022-09-03 13:36:48 +0900

    deprecated된 WebSecurityConfigurerAdapter 수정

--- src/main/java/com/mycustomblog/blog/config/SecurityConfig.java
+++ src/main/java/com/mycustomblog/blog/config/SecurityConfig.java
@@ -4,38 +4,37 @@ import com.mycustomblog.blog.config.exception.LoginFailHandler;
 import com.mycustomblog.blog.domain.Role;
 import com.mycustomblog.blog.service.PrincipalServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
+public class SecurityConfig{
     @Autowired
     private final PrincipalServiceImpl principalServiceImpl = null;
     @Autowired
     private final LoginFailHandler loginFailHandler = null;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        System.out.println("testsetest");
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable(); //토큰 X
         http.authorizeHttpRequests().antMatchers("/admin/*").hasRole(Role.ADMIN.name())
                 .anyRequest().permitAll()
-                .and()
+            .and()
                 .formLogin()
                 .loginPage("/login")
-                .and()
+            .and()
                 .logout()
                 .logoutSuccessUrl("/")
-                .and()
+            .and()
                 .oauth2Login()
                 .loginPage("/login")
                 .failureHandler(loginFailHandler)
                 .userInfoEndpoint()
                 .userService(principalServiceImpl);
+        return http.build();
     }
 }
