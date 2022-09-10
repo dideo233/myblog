287c2c734df9864bc85e877ccec9da4132f565ea
dideo233 <61315671+dideo233@users.noreply.github.com>
2022-09-10 20:51:25 +0900

    CI/CD (배포 자동화 구성) 다시 시도

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,6 +28,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-db.properties \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/myblog/application-oauth.properties,/home/ec2-user/app/myblog/application-db.properties \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
