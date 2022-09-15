ae663bf2f094bed0b71cf4a512a15e36932f91ff
dideo233 <61315671+dideo233@users.noreply.github.com>
2022-09-16 00:22:18 +0900

    배포 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ REPOSITORY=/home/ec2-user/app3/myblog/
 PROJECT_NAME=myblog
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
--- src/main/java/com/mycustomblog/blog/service/ArticleService.java
+++ src/main/java/com/mycustomblog/blog/service/ArticleService.java
@@ -75,7 +75,7 @@ public class ArticleService {
 
     //디폴트 섬네일
     private String makeDefaultThumb(String thumbnailUrl) {
-        String defaultThumbUrl = "https://raw.githubusercontent.com/dideo233/imageRepo/main/image/f69280db-1efc-41cf-9710-4087ed1fa573.jpg";
+        String defaultThumbUrl = "https://raw.githubusercontent.com/dideo233/imgRepo/main/image/e3b9569d-6837-4dc7-8b57-ee7333c95b2b.jpg";
         if (thumbnailUrl == null || thumbnailUrl.equals("")) {
             thumbnailUrl = defaultThumbUrl;
         }
